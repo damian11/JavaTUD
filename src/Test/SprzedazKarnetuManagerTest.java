3b3ada1524e728cb4625bc4126c32aa4a5446762
damian11 <damian.kasztelan1@op.pl>
2015-11-06 10:52:49 +0100

    up
    naprawa żeby testy działały

---
+++ src/Test/SprzedazKarnetuManagerTest.java
@@ -0,0 +1,115 @@
+package Test;
+
+
+	import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+	import main.Karnet;
+import main.Klient;
+import main.SprzedazKarnetu;
+
+	import org.junit.Test;
+
+	import service.KarnetManager;
+import service.KlientManager;
+	
+import service.SprzedazKarnetuManager;
+
+	public class SprzedazKarnetuManagerTest {
+	SprzedazKarnetuManager SprzedazKarnetuManager = new SprzedazKarnetuManager();
+	KlientManager KlientManager = new KlientManager();
+	KarnetManager KarnetManager = new KarnetManager();
+	
+	
+		
+
+    private final static String IMIE_1 = "Jurand";
+    private final static String NAZWISKO_1 = "Spychowski";
+    private final static long TELEFON_1 = 600100100;
+    
+    private final static String RODZAJ_1 = "OPEN";
+    private final static String OPIS_1 = "Karnet pozwala na: korzystanie z wszystkich stref fitnnes, uczestniczenie we wszystkich zajeciach, oraz gwarantuje 2 treningi personalne miesiêcznie";
+    private final static double CENA_1 = 250;
+    private final static double DELTA = 1e-15;
+
+  
+	     
+	@Test
+		public void sprawdzPolaczenie() {
+			assertNotNull(SprzedazKarnetuManager.getConnection());
+		}
+
+	
+	@Test
+	public void sprawdzDodajKarnet() {
+	
+	Karnet karnet = new Karnet (RODZAJ_1, OPIS_1, CENA_1);
+	
+	KarnetManager.wyczyscKarnety();
+	assertEquals(1, KarnetManager.dodajKarnet(karnet));
+	
+	List<Karnet> karnety = KarnetManager.PobierzKarnety();
+	Karnet pobieranieKarnetu = karnety.get(0);
+	
+	assertEquals(RODZAJ_1, pobieranieKarnetu.getRodzaj());
+	assertEquals(OPIS_1, pobieranieKarnetu.getOpis());
+	assertEquals(CENA_1, pobieranieKarnetu.getCena(),DELTA);
+	 
+	}
+	
+	
+	@Test
+	public void sprawdzDodajKlienta() {
+	
+	Klient klient = new Klient (IMIE_1, NAZWISKO_1, TELEFON_1);
+	
+	KlientManager.wyczyscKlientow();
+	assertEquals(1,KlientManager.dodajKlienta(klient));
+	
+	List<Klient> klienci = KlientManager.PobierzKlientow();
+	Klient pobieranieKlienta = klienci.get(0);
+	
+	assertEquals(IMIE_1, pobieranieKlienta.getImie());
+	assertEquals(NAZWISKO_1, pobieranieKlienta.getNazwisko());
+	assertEquals(TELEFON_1, pobieranieKlienta.getTelefon());
+	}
+	
+	
+	
+	
+	
+	@Test
+		public void sprawdzDodajSprzedazKarnetu() {
+		
+		Klient klient = new Klient (IMIE_1, NAZWISKO_1, TELEFON_1);
+		
+		KlientManager.wyczyscKlientow();
+		assertEquals(1,KlientManager.dodajKlienta(klient));
+		
+		List<Klient> klienci = KlientManager.PobierzKlientow();
+		Klient pobieranieKlienta = klienci.get(0);
+		
+		
+		Karnet karnet = new Karnet (RODZAJ_1, OPIS_1, CENA_1);
+		
+		KarnetManager.wyczyscKarnety();
+		assertEquals(1, KarnetManager.dodajKarnet(karnet));
+		
+		List<Karnet> karnety = KarnetManager.PobierzKarnety();
+		Karnet pobieranieKarnetu = karnety.get(0);
+		
+		SprzedazKarnetu sprzedazKarnetu  = new SprzedazKarnetu ();
+		
+		
+		
+
+
+		assertEquals(1, SprzedazKarnetuManager.dodajSprzedazKarnetu(pobieranieKlienta, pobieranieKarnetu));
+
+	
+	}
+	}
+
--- src/main/SprzedazKarnetu.java
+++ src/main/SprzedazKarnetu.java
@@ -5,21 +5,16 @@ public class SprzedazKarnetu {
 	private long id_sprzedazKarnetu;
 	private long id_klient;
 	private long id_karnet;
-	private String notatka = "brak";
+	
 	
 	
 	
 	public SprzedazKarnetu(){
-		super();
+	
 	}
 
-	public SprzedazKarnetu(long id_klient, long id_karnet, String notatka){
-		super();
-		this.id_karnet = id_karnet;
-		this.id_klient = id_klient;
-		this.notatka = notatka;
-	}
-	public static long getId_klient(){
+
+	public  long getId_klient(){
 		return id_klient;
 	}
 	
@@ -28,7 +23,7 @@ public class SprzedazKarnetu {
 		
 	}
 	
-	public static long getId_karnet(){
+	public  long getId_karnet(){
 		return id_karnet;
 	}
 	
@@ -36,13 +31,8 @@ public class SprzedazKarnetu {
 		this.id_karnet = id_karnet;
 	}
 	
-	public static String getNotatka(){
-		return notatka;
-	}
+
 	
-	public void setNotatka(String notatka){
-		this.notatka = notatka;
-	}
 
 	public long getId_sprzedazKarnetu() {
 		return id_sprzedazKarnetu;
